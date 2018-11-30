c2acbf954f80fe2a22b1a4794dd20d679406e46e
adidoank <42825332+adidoank@users.noreply.github.com>
2018-11-30 12:57:41 +0700

    Update user.php

--- user.php
+++ user.php
@@ -2,5 +2,6 @@
 $data= [
 'adi bordir','Adi Digitizer','Dzi','semua andinya','sungging','asep kanceuh','putripesek','nubie305','ateng','Bal Lack','Maulana Setiawan','suga zora','Thauvan GT','yassar sha\'id shafi\'i','Moezza 01','စုံလင်သော အလ','hasan coc','Pro Master','hasan abut','gan bro','Yudi Pratmaja','Gembel Elite','imel telu','SkaLova','Badrun','Nhóc Con Sr','Delwin Yoanes',
 'Beuti Lavea','MasterCraft Vn','Ari Sandi','I love Cartoon','Kaboom','Robert Simangunsong','Mohamad Husn',
+'Lucy Thien',
         ];
 $ydnews = ['tes',];
