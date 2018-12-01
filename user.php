cb6651252934a899fd3e78dd758ae87bbae17c68
adidoank <42825332+adidoank@users.noreply.github.com>
2018-12-01 21:24:18 +0700

    Update user.php

--- user.php
+++ user.php
@@ -6,7 +6,8 @@ $msgwe = "[!] enjoy your life";
 $data= [
 'adi bordir','Adi Digitizer','Dzi','semua andinya','sungging','asep kanceuh','putripesek','nubie305','ateng','Bal Lack','Maulana Setiawan','suga zora','Thauvan GT','yassar sha\'id shafi\'i','Moezza 01','စုံလင်သော အလ','hasan coc','Pro Master','hasan abut','gan bro','Yudi Pratmaja','Gembel Elite','imel telu','SkaLova','Badrun','Nhóc Con Sr','Delwin Yoanes',
 'Beuti Lavea','MasterCraft Vn','Ari Sandi','I love Cartoon','Kaboom','Robert Simangunsong','Mohamad Husn',
-'Lucy Thien','Adi Trader Demo','Mif Takhul','Feyy',
+'Lucy Thien','Adi Trader Demo','Mif Takhul','Feyy','I Dewa Gede Superatikadewa',
+
         ];
 $ydnews = ['tes',];
 $toto = ['tes',];
