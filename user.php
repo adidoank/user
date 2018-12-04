0ba5527bb558df6cf4a805a68b1dedf2cb0b774c
adidoank <42825332+adidoank@users.noreply.github.com>
2018-12-04 20:51:52 +0700

    Update user.php

--- user.php
+++ user.php
@@ -5,8 +5,7 @@ $msgwe = "[!] enjoy your life";
 $we = ["U84795",
   ];
 $data= [
-//'adi bordir',
-'Adi Digitizer','Dzi','semua andinya','sungging','asep kanceuh','putripesek','nubie305','ateng','Bal Lack','Maulana Setiawan','suga zora','Thauvan GT','yassar sha\'id shafi\'i','Moezza 01','စုံလင်သော အလ','hasan coc','Pro Master','hasan abut','gan bro','Yudi Pratmaja','Gembel Elite','imel telu','SkaLova','Badrun','Nhóc Con Sr','Delwin Yoanes',
+'adi bordir','Adi Digitizer','Dzi','semua andinya','sungging','asep kanceuh','putripesek','nubie305','ateng','Bal Lack','Maulana Setiawan','suga zora','Thauvan GT','yassar sha\'id shafi\'i','Moezza 01','စုံလင်သော အလ','hasan coc','Pro Master','hasan abut','gan bro','Yudi Pratmaja','Gembel Elite','imel telu','SkaLova','Badrun','Nhóc Con Sr','Delwin Yoanes',
 'Beuti Lavea','MasterCraft Vn','Ari Sandi','I love Cartoon','Kaboom','Robert Simangunsong','Mohamad Husn',
 'Lucy Thien','Adi Trader Demo','Mif Takhul','Feyy','I Dewa Gede Superatikadewa','Wendik','Qohwah',
 'Asmuri Aas','Goril Aas','Wili Aas','Gery Aas',
@@ -23,10 +22,11 @@ $data= [
 'Yoviar Pauzi','Ayesha Advocate',
 //vipm
 'HARIS Denzlot','Raihan Chainago','Bejo Jo206','Bejo Rusak','Jordan Donathan','Bejo Mace','Jojo Vake','Deaso Bejo','Lungkik Cuiru','Riyan Kartika','Lusto Rusak','Jorsn Rusak','Jordwun jerdin','Curit Astjx','Hais Suis',
+//mpiv
 'Gem Bel','Hati Yang Terluka','Umi Kholiifah',
 'Aditiya Sarofi Putra','NOOB GAMING','phim lẻ','Jordwun lrus','gan bro','Gan Bro','bot siji',
 'bot loro','bot telu','yu di','aryant259','Janandito W',
-'arifin','Noob Player','yoyomamam90@gmail.com','Andi Aas',
+'arifin','Noob Player','yoyomamam90@gmail.com','Andi Aas','elang aja','Muhammad paijo','Sutejo','Siti Azizah','Dian kartika',
         ];
 $ydnews = ['tes',];
 $toto = ['tes',];
