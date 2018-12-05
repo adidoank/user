b4d140c84ccc0b203dd81afa09603eb6cfed158a
adidoank <42825332+adidoank@users.noreply.github.com>
2018-12-05 19:24:35 +0700

    Update user.php

--- user.php
+++ user.php
@@ -2,10 +2,10 @@
 $msgtoto = "[!] tidak di ijinkan"."\n"."[!] menjual skrip ini kepada siapapun"."\n"."[!] msg by adi digitizer";
 $msgyd = "[!] enjoy your life";
 $msgwe = "[!] enjoy your life";
-$we = ["U84795",
-  ];
+
 $data= [
-'adi bordir','Adi Digitizer','Dzi','semua andinya','sungging','asep kanceuh','putripesek','nubie305','ateng','Bal Lack','Maulana Setiawan','suga zora','Thauvan GT','yassar sha\'id shafi\'i','Moezza 01','စုံလင်သော အလ','hasan coc','Pro Master','hasan abut','gan bro','Yudi Pratmaja','Gembel Elite','imel telu','SkaLova','Badrun','Nhóc Con Sr','Delwin Yoanes',
+//'adi bordir',
+'Adi Digitizer','Dzi','semua andinya','sungging','asep kanceuh','putripesek','nubie305','ateng','Bal Lack','Maulana Setiawan','suga zora','Thauvan GT','yassar sha\'id shafi\'i','Moezza 01','စုံလင်သော အလ','hasan coc','Pro Master','hasan abut','gan bro','Yudi Pratmaja','Gembel Elite','imel telu','SkaLova','Badrun','Nhóc Con Sr','Delwin Yoanes',
 'Beuti Lavea','MasterCraft Vn','Ari Sandi','I love Cartoon','Kaboom','Robert Simangunsong','Mohamad Husn',
 'Lucy Thien','Adi Trader Demo','Mif Takhul','Feyy','I Dewa Gede Superatikadewa','Wendik','Qohwah',
 'Asmuri Aas','Goril Aas','Wili Aas','Gery Aas',
@@ -29,7 +29,7 @@ $data= [
 'arifin','Noob Player','yoyomamam90@gmail.com','Andi Aas','elang aja','Muhammad Paijo','Sutejo','Siti Azizah','Dian Kartika','Budi Aas','Raby Aas',
 'Wendy Aas','Rani Aas','Lê Hoàng','Rere Aas','Rony Aas',
 'Riki Aas','Junaidi Abdulah','Tidak Dikenal','Agung Nugraha','SobLok DanSka VoltHz','Afil X-friends Discography Hitam-Putih','Aan TheRG','Davin Hardit','robert simangunsong','Restu OnGame','santa lidwyna tarigan','NEWBIE BERKELAS','Bambang Setiawan',
-'Mr. Benk','Yafia Ah','Yafiah Tri Ramdani wijaya','nayz stone','Myvi Com','Seda Coeg','Wayan','Kiki Albani',
+'Mr. Benk','Yafia Ah','Yafiah Tri Ramdani wijaya','nayz stone','Myvi Com','Seda Coeg','Wayan','Kiki Albani','U84795',
         ];
 $ydnews = ['tes',];
 $toto = ['tes',];
