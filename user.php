b62da41034b81b01d42881d96aa1974b5d318f29
adidoank <42825332+adidoank@users.noreply.github.com>
2018-12-07 01:34:33 +0700

    Update user.php

--- user.php
+++ user.php
@@ -8,8 +8,7 @@ $msgyd = "[!] enjoy your life";
 $msgwe = "[!] enjoy your life";
 
 $data= [
-'adi bordir',
-'Adi Digitizer','Dzi','semua andinya','sungging','asep kanceuh','putripesek','nubie305','ateng','Bal Lack','Maulana Setiawan','suga zora','Thauvan GT','yassar sha\'id shafi\'i','Moezza 01','စုံလင်သော အလ','hasan coc','Pro Master','hasan abut','gan bro','Yudi Pratmaja','Gembel Elite','imel telu','SkaLova','Badrun','Nhóc Con Sr','Delwin Yoanes',
+'adi bordir','Adi Digitizer','Dzi','semua andinya','sungging','asep kanceuh','putripesek','nubie305','ateng','Bal Lack','Maulana Setiawan','suga zora','Thauvan GT','yassar sha\'id shafi\'i','Moezza 01','စုံလင်သော အလ','hasan coc','Pro Master','hasan abut','gan bro','Yudi Pratmaja','Gembel Elite','imel telu','SkaLova','Badrun','Nhóc Con Sr','Delwin Yoanes',
 'Beuti Lavea','MasterCraft Vn','Ari Sandi','I love Cartoon','Kaboom','Robert Simangunsong','Mohamad Husn',
 'Lucy Thien','Adi Trader Demo','Mif Takhul','Feyy','I Dewa Gede Superatikadewa','Wendik','Qohwah',
 'Asmuri Aas','Goril Aas','Wili Aas','Gery Aas',
@@ -32,9 +31,12 @@ $data= [
 'bot loro','bot telu','yu di','aryant259','Janandito W',
 'arifin','Noob Player','yoyomamam90@gmail.com','Andi Aas','elang aja','Muhammad Paijo','Sutejo','Siti Azizah','Dian Kartika','Budi Aas','Raby Aas',
 'Wendy Aas','Rani Aas','Lê Hoàng','Rere Aas','Rony Aas',
-'Riki Aas','Junaidi Abdulah','Tidak Dikenal','Agung Nugraha','SobLok DanSka VoltHz','Afil X-friends Discography Hitam-Putih','Aan TheRG','Davin Hardit','robert simangunsong','Restu OnGame','santa lidwyna tarigan','NEWBIE BERKELAS','Bambang Setiawan',
-'Mr. Benk','Yafia Ah','Yafiah Tri Ramdani wijaya','nayz stone','Myvi Com','Seda Coeg','Wayan','Kiki Albani','Kresna Rizkie','itel meh','gretonger Tool','bball','Remsha Rajpout','Khalifi Nugraha','Hasan Ahmmad YK','Ahmad_alfian','bondex pay','Mona Xperia','BangGenzzot Gaming','Addinansah Azmil Hidayat',
-'Elza Mila','astrajingga18','Astrajingga','Chiko Andrea','Thoriq',
+'Riki Aas','Junaidi Abdulah','Tidak Dikenal','Agung Nugraha','SobLok DanSka VoltHz','Afil X-friends Discography Hitam-Putih','Aan TheRG','Davin Hardit',
+'robert simangunsong','Restu OnGame','santa lidwyna tarigan','NEWBIE BERKELAS','Bambang Setiawan',
+'Mr. Benk','Yafia Ah','Yafiah Tri Ramdani wijaya',
+'nayz stone','Myvi Com','Seda Coeg','Wayan','Kiki Albani','Kresna Rizkie','itel meh','gretonger Tool','bball','Remsha Rajpout','Khalifi Nugr','Hasan Ahmmad YK','Ahmad_alfian','bondex pay','Mona Xperia',
+'BangGenzzot Gaming','Addinansah Azmil Hidayat','Elza Mila','astrajingga18','Astrajingga','Chiko Andrea',
+'Thoriq','Agus Saepuloh','Muhammad Feb',
         ];
 $ydnews = ['tes',];
 $toto = ['tes',];
