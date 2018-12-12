315e80e4eebdb35a99ce4ac8ce20ada4a537c2bb
adidoank <42825332+adidoank@users.noreply.github.com>
2018-12-13 05:48:54 +0700

    Update user.php

--- user.php
+++ user.php
@@ -3,7 +3,7 @@ echo $red."[!] warning !!!".$t;
 echo $kuning."[-] ini adalah program ilegal".$t;
 echo $kuning."[-] resiko sepenuhnya di tanggung pengguna".$t.$ijo.$t;
 
-$msgtoto = "[!] subscribe channel sungging gtu 😆"."\n"."[!] atau chanel adi bordir guys"."\n"."[!] dijamin ori ";
+$msgtoto = "[!] info toto news berganti menjadi TTNews".$t."[-] gunakan akun toto news untuk login di TTNews".$t."[-] skrip tetap work dengan app TTNews".$t."[-] cara tranfer akun login di TTNews pilih old user masukan kode invite akun toto news tranfer akun selesai";
 $msgyd = "[!] enjoy your life";
 $msgwe = "[!] info withdraw work with delay 10 sec";
 $msgveeu = "[!] enjoy your life";
