12e3a3663052ba734d489bcfcb35b633947a2614
adidoank <42825332+adidoank@users.noreply.github.com>
2018-12-16 01:38:03 +0700

    Update user.php

--- user.php
+++ user.php
@@ -2,7 +2,7 @@
 echo $red."[!] warning !!!".$t;
 echo $kuning."[-] ini adalah program ilegal".$t;
 echo $kuning."[-] resiko sepenuhnya di tanggung pengguna".$t.$ijo.$t;
-
+$ads= $putih."[÷]    official channel    [÷]".$t.$ijo."[-] adi bordir ".$putih."| atau |".$ijo." sungging [-]";
 $msgtoto = "[!] enjoy your life";
 $msgyd = "[!] enjoy your life";
 $msgwe = "[!] enjoy your life";
