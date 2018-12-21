a2074a103c53f3257192da67fb506f9a6fb5f975
adidoank <42825332+adidoank@users.noreply.github.com>
2018-12-21 19:21:55 +0700

    Update user.php

--- user.php
+++ user.php
@@ -3,7 +3,7 @@ echo $red."[!] warning !!!".$t;
 echo $kuning."[-] ini adalah program ilegal".$t;
 echo $kuning."[-] resiko sepenuhnya di tanggung pengguna".$t.$ijo.$t;
 $ads= $putih."[÷]    official channel    [÷]".$t.$ijo."[-] adi bordir ".$putih."| atau |".$ijo." sungging [-]";
-$msgtoto = "[!] Subscribe Youtube adi bordir dan Sungging".$t.$red."[°] demi keamanan hentikan aktifitas bot sekarang !!!!".$kuning."[•] ctrl + c";
+$msgtoto = "[!] Subscribe Youtube adi bordir dan Sungging".$t.$red."[°] demi keamanan hentikan aktifitas bot sekarang !!!!".$t.$kuning."[•] ctrl + c";
 $msgyd = "[!] update [v.1.2]";
 $msgwe = "[!] enjoy your life";
 $msgveeu = "[!] enjoy your life";
