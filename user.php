8e97b2d0c28224a33f2186b4016866a9fe306215
adidoank <42825332+adidoank@users.noreply.github.com>
2018-12-31 09:28:52 +0700

    w

--- user.php
+++ user.php
@@ -15,7 +15,7 @@ $msgwe = "[!] oops sorry silahkan cek billboard w&e".$t.$red."[•] demi keamana
 $msgveeu = "[!] enjoy your life";
 
 $data= [
-'adi bordir','L41023',
+'adi bordir','L41023','adi doank',
  'Adi Digitizer','Dzi','semua andinya','sungging','asep kanceuh','putripesek','nubie305','ateng','Bal Lack','Maulana Setiawan','suga zora','Thauvan GT','yassar sha\'id shafi\'i','Moezza 01','စုံလင်သော အလ','hasan coc','Pro Master','hasan abut','gan bro','Yudi Pratmaja','Gembel Elite','imel telu','SkaLova','Badrun','Nhóc Con Sr','Delwin Yoanes',
 'Beuti Lavea','MasterCraft Vn','Ari Sandi','I love Cartoon','Kaboom','Robert Simangunsong','Mohamad Husn',
 'Lucy Thien','Adi Trader Demo','Mif Takhul','Feyy','I Dewa Gede Superatikadewa','Wendik','Qohwah',
