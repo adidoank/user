92f54598d9a36d811b1901d74a83218126292d3f
adidoank <42825332+adidoank@users.noreply.github.com>
2019-01-18 04:56:04 +0700

    Update user.php

--- user.php
+++ user.php
@@ -2,6 +2,7 @@
 echo $red."[!] warning !!!".$t;
 echo $kuning."[-] ini adalah program ilegal".$t;
 echo $kuning."[-] resiko sepenuhnya di tanggung pengguna".$t.$ijo.$t;
+$stat_ads=true;
 $sungging = $putih.'[*] '.$ijo.'subcribe youtube sungging '.$putih.'[-] code: '.$ijo.'L41023';
 $ads= $putih."[÷]    official channel    [÷]".$t.$ijo."[-] adi bordir ".$putih."| atau |".$ijo." sungging [-]";
 
