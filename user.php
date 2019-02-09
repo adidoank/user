cfc578bf8d9175ed5891f1b7a58efe34f84b8966
adidoank <42825332+adidoank@users.noreply.github.com>
2019-02-09 09:49:58 +0700

    ty

--- user.php
+++ user.php
@@ -4,7 +4,10 @@ echo $kuning."[-] ini adalah program ilegal".$t;
 echo $kuning."[-] resiko sepenuhnya di tanggung pengguna".$t.$ijo.$t;
 $stat_ads=true;
 $sungging = $putih.'[*] '.$ijo.'subcribe youtube sungging '.$putih.'[-] code: '.$ijo.'I46346';
-$ads= $putih.'[*] '.$ijo.'subcribe youtube sungging '.$putih.'[-] code: '.$ijo.'I46346'.$t.$putih."[÷]    official channel    [÷]".$t.$ijo."[-] adi bordir ".$putih."| atau |".$ijo." sungging [-]";
+$ads= $putih.'[*] '.$ijo.'subcribe youtube sungging '.$putih.'[-] code: '.$ijo.'I46346'.$t.$putih."[÷]    official channel    [÷]".$t.$ijo."[-] adi bordir ".$putih."| atau |".$ijo." sungging [-]".$t."[•] sekilas info".$t.
+"[•] w&e berganti menjadi vesea".$t.
+"[•] silahkan download di playstore".$t.
+"[•] https://play.google.com/store/apps/details?id=com.sven.taoshijie.international".$t; 
 
 $msgtoto = "[!] Subscribe Youtube adi bordir dan Sungging".$t.$ijo."[•••••] sekilas info [••••••]
 
