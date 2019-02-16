2cc056a082dcece59254b8ad4492ac14dc7785ba
adidoank <42825332+adidoank@users.noreply.github.com>
2019-02-17 06:18:04 +0700

    updt

--- user.php
+++ user.php
@@ -3,48 +3,10 @@ echo $red."[!] warning !!!".$t;
 echo $kuning."[-] ini adalah program ilegal".$t;
 echo $kuning."[-] resiko sepenuhnya di tanggung pengguna".$t.$ijo.$t;
 $stat_ads=true;
-$sungging = $putih.'[*] '.$ijo.'subcribe youtube sungging '.$putih.'[-] code: '.$ijo.'I46346';
-$ads= $putih.'[*] '.$ijo.'subcribe youtube sungging '.$t.$putih."[÷]    official channel    [÷]".$t.$ijo."[-] adi bordir ".$putih."| atau |".$ijo." sungging [-]".$t."[•] sekilas info".$t.
-"[•] wait for update w&e >> vesea".$t; 
 
-$msgtoto = "[!] Subscribe Youtube adi bordir dan Sungging".$t.$ijo."[•••••] sekilas info [••••••]
+$ads= $putih.'[*] '.$ijo.'subcribe youtube sungging '.$t.$putih."[÷]    official channel    [÷]".$t.$ijo."[-] adi bordir ".$putih."| atau |".$ijo." sungging [-]".$t; 
 
-hallo guys apa kabar
-yang masih setia menunggu update script terbaru toto
-sedikit sy jelaskan perjalanan kita
->> toto berganti menjadi TTNews 
->> TTNews berganti menjadi watch news
->> watch news berganti menjadi cworld
-
-nah saat ini cworld masih dalam tahap beta tes
-untuk yg blm punya apk cworld bisa di download
-
-di sini >> https://sfile.mobi/8ZPF2b181Ut
-
-atau kunjungi playstore
-di sini >> http://news.masjmzs.com/index/openredpacket?unique=e1d4bac1b720a2af7c0acf3d8f81617c&d=31&lng=in
- 
-dan disini >> 
-https://play.google.com/store/apps/details?id=com.news.newsyayd
-
-setelah menginstall aplikasi terbaru cworld
-silahkan login menggunakan akun favorit kalian
-dan ketika di minta pilihan
- >> new user atau old user <<
-silahkan pilih new user untuk menghilangkan jejak kita
-setelah login berhasil masuk ke tab profil
-di kanan bawah dan pergi untuk masukan kode undangan
-silahkan memasukan kode >> A15280 <<
-
-skrip akan sy update setelah close beta tes
-atau setelah cworld di rillis
-jika sebelum rillis ada 200+ user
-menggunakan kode saya maka skrip akan sy buat free
-tapi jika kurang dari itu
-maka system validasi akan berlaku seperti watch earn
-
-demikian sekilas info$putih
-msg_by:$ijo adidoank";
+$msgtoto = "[!] Subscribe Youtube adi bordir dan Sungging".$t.$ijo."[•••••] sekilas info [••••••]";
 
 $msgyd = "[!] ganguan pada server jangan panik ini tidak lama";
 $yd_stat = true;
@@ -52,7 +14,7 @@ $vip1= false;
 //$weblock =["Imam Almasyah Risanzani",];
 $ydblock = ["Imam Almasyah Risanzani",];
 $we_stat = true;
-$msgwe = "[•] enjoy your life ";
+$msgwe = "[•] update wne  vesea 3.0.1 ready";
 $msgveeu = "[!] enjoy your life";
 
 $data= ['H84391','C19302',
