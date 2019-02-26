0cd207ef59a0f28bcd53bc773ccf599fa0162601
adidoank <42825332+adidoank@users.noreply.github.com>
2019-02-27 00:42:06 +0700

    gah

--- user.php
+++ user.php
@@ -15,7 +15,7 @@ $vip1= false;
 $ydblock = ["Imam Almasyah Risanzani",];
 $we_stat = true;
 
-$msgwe = "[•] update wne  vesea 3.0.1 ready".$t.$kuning."[•] untuk menjadi member gunakan kode invite di atas👆👆".$t;
+$msgwe = "[•] update member tertunda karena server down".$t.$kuning."[•] untuk menjadi member gunakan kode invite di atas👆👆".$t;
 $donasi=true;
 $msgveeu = "[!] update skrip veeu".$t."[•] git clone https://github.com/adidoank/veeu";
 
